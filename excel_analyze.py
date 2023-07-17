e6bdbf808c8975f2bc787093f9672ec0d27708ab
DESKTOP-VSLBRN9\LEEDONGSU-N <lds961006@gmail.com>
2023-07-17 14:46:23 +0900

    ui에서 버튼 클릭을 작성가능한지 확인

--- excel_analyze.py
+++ excel_analyze.py
@@ -1,8 +1,8 @@
 import os, pandas, win32com.client, openpyxl, xlrd
 
-def checking_excel_encrypted(full_filepath):
+
+def is_encrypted_excel(full_filepath):
     if full_filepath.find('.xlsx') != -1:
-        # xlsx 확장자 파일에 대해 암호화 확인
         try:
             xl = openpyxl.load_workbook(full_filepath, read_only=True)
             xl.close()
@@ -19,11 +19,7 @@ def checking_excel_encrypted(full_filepath):
             return True
 
 def uf_excel_reader(full_filepath, bank_name):
-    '''
-    매개변수로 받은 엑셀에 존재하는 이름, 금액을 추출하여
-    결측치를 제거하고, 병합하여 하나의 데이터 프레임으로 리턴
-    '''
-    if checking_excel_encrypted(full_filepath) == False:
+    if is_encrypted_excel(full_filepath) == False:
         df_temp = pandas.read_excel(full_filepath, header=None)
         data_start_row = 0
         money_col = 0
@@ -36,13 +32,13 @@ def uf_excel_reader(full_filepath, bank_name):
             data_start_row = 7
             money_col = 4
             name_col = 5
-        r_money = (df_temp[data_start_row:][money_col]) # 금액
-        r_name = (df_temp[data_start_row:][name_col]) # 이름
-        test = pandas.concat([r_name, r_money], axis=1)
-        test.reset_index(inplace=True, drop=True)
-        test.rename(columns={name_col:'이름', money_col:'금액'}, inplace=True)
-        test = test.dropna(axis=0) # 결측치 제거
-        return test
+        r_money = (df_temp[data_start_row:][money_col])
+        r_name = (df_temp[data_start_row:][name_col])
+        df_data = pandas.concat([r_name, r_money], axis=1)
+        df_data.reset_index(inplace=True, drop=True)
+        df_data.rename(columns={name_col:'이름', money_col:'금액'}, inplace=True)
+        df_data = df_data.dropna(axis=0)
+        return df_data
     else:
         xlApp = win32com.client.Dispatch("Excel.Application")
         xlApp.Visible = False
@@ -51,51 +47,41 @@ def uf_excel_reader(full_filepath, bank_name):
         try:
             book = xlApp.Workbooks.Open(full_filepath, False, True, None, excel_password)
             ws = book.ActiveSheet
+            temp_dict = {}
+            temp_namelist = []
+            temp_moneylist = []
             for i in range(12, 38):
-                print(ws.Cells(i, 4).Value, ws.Cells(i, 7).Value)
+                if ws.Cells(i, 7).Value in temp_dict.keys():
+                    temp_namelist.append(ws.Cells(i, 7).Value + str(i))
+                    temp_moneylist.append(ws.Cells(i, 4).Value)
+                else:
+                    temp_namelist.append(ws.Cells(i, 7).Value)
+                    temp_moneylist.append(ws.Cells(i, 4).Value)
+            temp_dict['이름'] = temp_namelist
+            temp_dict['금액'] = temp_moneylist
+            df_data = pandas.DataFrame(data = temp_dict)
             xlApp.Quit()
-            return 0
+            return df_data
         except Exception as e:
             if str(e).find('암호가 잘못되었습니다.') != -1:
-                print('비밀번호가 틀렸습니다.')
+                print('Invalid password.')
             else:
-                print('uf_excel_reader 오류 : ', e)
+                print('uf_excel_reader error : ', e)
                 
+# A function that takes a dataframe as a parameter and converts it to an Excel file.
 def uf_excel_writer(df_data):
     if os.path.isfile('result_data.xlsx'):
-        writer = pandas.ExcelWriter('result_data.xlsx', mode='a', engine='openpyxl', if_sheet_exists='overlay')
+        writer = pandas.ExcelWriter(path = 'result_data.xlsx', mode='a', engine='openpyxl', if_sheet_exists='overlay')
         max_row = writer.sheets['sheet1'].max_row
-        
         df_data.to_excel(writer, sheet_name='sheet1', startcol=0, startrow=max_row, index=False, encoding='utf-8', header=None)
         writer.save()
         writer.close()  
     else:
         df_data.to_excel(excel_writer='result_data.xlsx', sheet_name='sheet1', index=False, encoding='utf-8')
-
-    # if type(df_data) == int:
-    #     print('엥')
-    # try:
-    #     writer = pandas.ExcelWriter('result_data.xlsx', mode = 'a', if_sheet_exists='overlay', engine='openpyxl')
-    #     last_row = writer.sheets['sheet'].max_row
-    #     if last_row == 1:
-    #         type(df_data)
-    #         df_data.to_excel(writer, sheet_name = 'sheet', index = False, encoding = 'utf-8', startrow = 0, startcol = 0)
-    #     else:
-    #         type(df_data)
-    #         df_data.to_excel(writer, sheet_name = 'sheet', startrow = int(writer.sheets['sheet'].max_row), startcol = 0, index = False, header = None)
-    # except Exception as e:
-    #     if str(e).find('No such file') != -1:
-    #         print(f'엑셀이 없어 생성합니다.')
-    #         df_data.to_excel(excel_writer = 'result_data.xlsx', sheet_name = 'sheet', index = False)
-    #     else:
-    #         print(f'uf_excel_writer 오류 : {e}')
-            
-    writer.close()
-    
     
 def excel_analysis(dirpath):
     data_list = os.listdir(dirpath)
-    bank_name = '농협' # 파일명으로 은행 가를수 있는가?
+    bank_name = '농협'
 
     for file_list in data_list:
         if file_list.find('농협') != -1:
